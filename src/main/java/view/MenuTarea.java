d37eb51837d6442ecaa39618af3e1a8c901fe11e
saammuu <samuelfm63@gmail.com>
2024-01-31 11:44:57 +0100

    Commit random

--- src/main/java/view/MenuTarea.java
+++ src/main/java/view/MenuTarea.java
@@ -3,7 +3,7 @@ package view;
 import Controller.Controller;
 
 public class MenuTarea {
-	
+	//Commit random
 	public static void menuTarea(Controller controller) {
 		System.out.println("MENU TAREA");
 	}
