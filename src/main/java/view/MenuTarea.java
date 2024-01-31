69bd849212e0bbed574d22e54b7d2c8b56566d28
saammuu <samuelfm63@gmail.com>
2024-01-31 11:22:31 +0100

    Nueva rama tarea aarreglado y solucionado

--- src/main/java/view/MenuTarea.java
+++ src/main/java/view/MenuTarea.java
@@ -3,7 +3,7 @@ package view;
 import Controller.Controller;
 
 public class MenuTarea {
-	
+	//Aqui empezará el menu
 	public static void menuTarea(Controller controller) {
 		System.out.println("MENU TAREA");
 	}
